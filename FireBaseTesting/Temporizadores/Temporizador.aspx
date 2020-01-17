009b83134984da84a51782a64b3b1dcd7a259dec
wmartinez1984 <34170344+wmartinez1984@users.noreply.github.com>
2020-01-16 18:43:11 -0600

    Temporizador update

--- FireBaseTesting/Temporizadores/Temporizador.aspx
+++ FireBaseTesting/Temporizadores/Temporizador.aspx
@@ -10,22 +10,30 @@
 </head>
 <body>
 <!-- partial:index.partial.html -->
-<div class="shadow"></div>
-<div class="card">
-	<div style="width:50%;margin:auto;">
-		<p style="color:#ffffff">Línea de producción </p>
-        <div class="card__overlay"></div>
-		<div class="card__button" id="btnstart"></div>
-		<div class="card__counter">
+	<p style="color:#ff6a00">Línea de producción </p>
+
+	<div  class="divCard">
+		<div style="">
+		 <div class="shadow">
+
+		 </div>
+			<div class="card">
+				<div>
+				
+					<div class="card__overlay"></div>
+					<div class="card__button" id="btnstart"></div>
+					<div class="card__counter">
 			
-			<div id="num" class="card__counter__num">
-				5
+						<div id="num" class="card__counter__num">
+							5
+						</div>
+			
+					</div>		
+				</div>	
 			</div>
-			
-		</div>
+	  </div>
 	</div>
-	
-</div>
+		
 <!-- partial -->
   <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
   <script src='https://cdnjs.cloudflare.com/ajax/libs/react/0.13.0/react.min.js'></script>
--- FireBaseTesting/Temporizadores/js/Timer.js
+++ FireBaseTesting/Temporizadores/js/Timer.js
@@ -5,31 +5,31 @@ button.on('click', function clicked() {
 		
 	$(this).off('click', clicked);
 
-var count=5;
-var counter=setInterval(timer, 1000);
+	var count=5;
+	var counter=setInterval(timer, 1000);
 
-function timer()
-{
+	function timer()
+	{
  
-  count -= 1;
-  if (count === -1)
-  {
-  	clearInterval(counter);
+	  count -= 1;
+	  if (count === -1)
+	  {
+  		clearInterval(counter);
 		
-		setTimeout(function(){
-			count = 5;
-			document.getElementById("num").innerHTML=count;
+			setTimeout(function(){
+				count = 5;
+				document.getElementById("num").innerHTML=count;
 
-			button.removeClass('card__button--triggered');
-			button.on('click', clicked);
+				button.removeClass('card__button--triggered');
+				button.on('click', clicked);
 			
-		}, 1);
+			}, 1);
 
-     return;
-  }
-	document.getElementById("num").innerHTML = count;
-	document.getElementById('btnstart').addClass('card__button--triggered');
+		 return;
+	  }
+		document.getElementById("num").innerHTML = count;
+		document.getElementById('btnstart').addClass('card__button--triggered');
 	
-}
+	}
 	
 });
